7864ee3b4b1f2c4b861007f7c989b783b58dc6bb
Jacob <jacobt0722@gmail.com>
2026-05-29 16:14:00 -0700

    removed block assignment from comb logic

--- ALU.sv
+++ ALU.sv
@@ -150,32 +150,32 @@ end
 
 always_comb begin
     if(op_add) begin
-        aluOut <= adderOut;
-        carryOut <= adderCarry;
+        aluOut = adderOut;
+        carryOut = adderCarry;
     end
     else if(op_sub) begin
-        aluOut <= subOut;
-        carryOut <= subCarry;
+        aluOut = subOut;
+        carryOut = subCarry;
     end
     else if(op_and) begin
-        aluOut <= andOut;
-        carryOut <= 1'b0;
+        aluOut = andOut;
+        carryOut = 1'b0;
     end
     else if(op_or) begin
-        aluOut <= orOut;
-        carryOut <= 1'b0;
+        aluOut = orOut;
+        carryOut = 1'b0;
     end
     else if(op_xor) begin
-        aluOut <= xorOut;
-        carryOut <= 1'b0;
+        aluOut = xorOut;
+        carryOut = 1'b0;
     end
     else if(op_outA) begin
-        aluOut <= a;
-        carryOut <= 1'b0;
+        aluOut = a;
+        carryOut = 1'b0;
     end
     else if(op_outB) begin
-        aluOut <= b;
-        carryOut <= 1'b0;
+        aluOut = b;
+        carryOut = 1'b0;
     end
 end
 
