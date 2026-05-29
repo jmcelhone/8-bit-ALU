c61164031185e5c8516fea2a2d6b070e2ba4e49c
Jonah Smart <smartj@oregonstate.edu>
2026-05-29 14:49:25 -0700

    Added sign bit of second operand in adder.

--- Add.sv
+++ Add.sv
@@ -1,6 +1,7 @@
 module Add(
 input logic [7:0] a,
 input logic [7:0] b,
+input logic sign,
 output logic [7:0] sum,
 output logic carry
 );
@@ -9,16 +10,16 @@ logic [7:0] c;
 
 initial begin
 c = 8'h00;
-carry = 1'h0;
 end
 
 always_comb begin
 sum[0] = a[0]^b[0];
-c[0] = a[0]&b[0];
+c[0] = a[0]&(b[0]|sign) | ~b[0]&sign;
+//should probably make a truth table to back up the above line of code
 
 for(int i=0;i<7;i++)begin
-sum[i+1] = a[i]^b[i]^c[i];
-c[i+1] = a[i]&(b[i]|c[i]) | b[i]&c[i];
+sum[i+1] = a[i]^b[i]^c[i]^sign;
+c[i+1] = a[i]&((b[i]^sign)|c[i]) | (b[i]^sign)&c[i];
 end
 
 carry = c[7];
