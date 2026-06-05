a8db76d2f9e00b43bf1b288aeb225d16b1c4d1b8
Jeremy <jeremyhmcelhone@gmail.com>
2026-06-05 15:34:27 -0700

    remove output enable pins

--- Output.sv
+++ Output.sv
@@ -3,7 +3,6 @@ module Output (
     input logic         reset,
     input logic  [7:0]  in,
     input logic         nextStateButton,
-    input  logic        enable,
     output logic [6:0]  seg_high,
     output logic [6:0]  seg_low,
     output logic        overflow_led
@@ -23,15 +22,15 @@ ALU alu(
 );
 
     SevenSegmentDecode decode_high (
-        .digit    (enable ? result[7:4] : 4'h0),
+        .digit    (result[7:4]),
         .segments (seg_high)
     );
 
     SevenSegmentDecode decode_low (
-        .digit    (enable ? result[3:0] : 4'h0),
+        .digit    (result[3:0]),
         .segments (seg_low)
     );
 
-    assign overflow_led = enable ? overflow : 1'b0;
+    assign overflow_led = overflow;
 
 endmodule
